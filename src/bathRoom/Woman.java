fb6e4f9e22059755c8c0c7b1c66683fb015a3c1b
Jens Roderus <mongelonas@hotmail.com>
2019-11-21 20:10:10 +0100

    did stuff

--- src/bathRoom/Woman.java
+++ src/bathRoom/Woman.java
@@ -4,67 +4,40 @@ import java.util.concurrent.ThreadLocalRandom;
 import common.IUnisexBathroomActorProcess;
 import se.his.iit.it325g.common.AndrewsProcess;
 
-
 public class Woman implements Runnable, IUnisexBathroomActorProcess {
 
 	@Override
 	public void run() {
 		while (true) {
-			GlobalState.semMutex.P();
-			
-			if(GlobalState.numberOfWomenInCS > 0) {
-				GlobalState.semMutex.V();
-				GlobalState.semWoman.P();
-			}
-			
-			printState();
-			
-			GlobalState.numberOfWomenInCS++;
-			GlobalState.signal();
-			
 			doThings();
-			
-			GlobalState.semMutex.P();
-			GlobalState.numberOfWomenInCS--;
-			GlobalState.signal();
-			
-			/*
 			GlobalState.semMutex.P();
 			if(GlobalState.numberOfMenInCS == 0) {
-				if(GlobalState.numberOfWomenInCS == 0) { 
-					GlobalState.semWoman.P();
-				}
-				GlobalState.semMutex.V();
 				GlobalState.numberOfWomenInCS++;
+				GlobalState.semMutex.V();
+				doThings();
 				printState();
-				// Critical section
-				this.doThings();
-				// Exit protocol
+				GlobalState.semMutex.P();
 				GlobalState.numberOfWomenInCS--;
-				if(GlobalState.numberOfWomenInCS == 0) {
-					GlobalState.semWoman.V();
-				}
+				GlobalState.semMutex.V();
+			}else {
+				GlobalState.semMutex.V();
 			}
-			*/
 		}
 	}
 
-	// printout on the global state
+
 	@Override
 	public  void printState() {
 		System.out.println(this);
 	}
 
-	// represents that processes are staying in a state for a while
+
 	@Override
 	public void doThings() {
 		AndrewsProcess.uninterruptibleMinimumDelay(ThreadLocalRandom.current()
-				.nextInt(100, 200));
+				.nextInt(100, 500));
 	}
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
 		return  "W " + AndrewsProcess.currentAndrewsProcessId()
